1215009af98d9160d1f6b39a09a2036054a23062
Muharrem Midilli <mumidill@k1m23s07.42istanbul.com.tr>
2026-07-01 21:24:00 +0300

    dasdas

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -1,85 +1,39 @@
 
-vft_putchar(char c)
+#include <unistd.h>
+
+void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
+
+void ft_putaddr(unsigned long long addr)
+{
+    if (addr >= 16)
+        ft_putaddr(addr / 16);
+    ft_putchar("0123456789abcdef"[addr % 16]);
+}
+
 void ft_print_address(char *addr, unsigned int n)
 {
-    int i;
+    unsigned int i;
 
     i = 0;
     while(i < n - 1)
     {
-    ft_putchar(&addr[i]);
+    ft_putaddr(&addr[i]);
     i++;
     }
 }
 
-
 void *ft_print_memory(void *addr, unsigned int size)
 {
-    ft_print_address(addr);
+    ft_print_address(addr, size);
+    return (addr);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    unsigned int i;
-    unsigned int j;
-    unsigned char *ptr;
-
-    ptr = (unsigned char *)addr;
-    i = 0;
-    while (i < size)
-    {
-        j = 0;
-        while (j < 16 && i + j < size)
-        {
-            if (ptr[i + j] < 16)
-                write(1, "0", 1);
-            ft_putchar("0123456789abcdef"[ptr[i + j] / 16]);
-            ft_putchar("0123456789abcdef"[ptr[i + j] % 16]);
-            if (j % 2 == 1)
-                write(1, " ", 1);
-            j++;
-        }
-        while (j < 16)
-        {
-            write(1, "  ", 2);
-            if (j % 2 == 1)
-                write(1, " ", 1);
-            j++;
-        }
-        j = 0;
-        while (j < 16 && i + j < size)
-        {
-            if (ptr[i + j] >= ' ' && ptr[i + j] <= '~')
-                ft_putchar(ptr[i + j]);
-            else
-                ft_putchar('.');
-            j++;
-        }
-        write(1, "\n", 1);
-        i += 16;
-    }
+int main(void)
+{
+    char *str = "Hello, World!";
+    ft_print_memory(str, 13);
+    return (0);
 }
---
+++ c02/ex12/test.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putaddr(unsigned long long addr)
+{
+	if (addr > 16)
+	{
+		ft_putaddr(addr / 16);
+	}
+	ft_putchar("0123456789abcdef"[addr % 16]);
+}
+
+void	ft_print_address(char *addr, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		ft_putaddr(&addr[i]);
+		i++;
+	}
+}
+
+void	*ft_print_memory(char *addr, unsigned int size)
+{
+	ft_print_address(&addr, size);
+}
+
+int	main(void)
+{
+	char *str = "Hello, World!";
+	ft_print_memory(str, 13);
+	return (0);
+}
